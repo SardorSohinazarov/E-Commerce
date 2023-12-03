3afab435538507206d0f3570082445e5f01346e3
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-04 02:05:39 +0500

    Branch created

---
+++ src/E-Commerce.Domain/Entities/Branch.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Commerce.Domain.Entities
+{
+    public class Branch
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
+    }
+}
