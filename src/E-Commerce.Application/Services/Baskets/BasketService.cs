3cdaec2f471a8d17382be1ecf36cca0a206ba044
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-05 22:03:26 +0500

    Basket service implemented

---
+++ src/E-Commerce.Application/Services/Baskets/BasketService.cs
@@ -0,0 +1,53 @@
+﻿using E_Commerce.Application.Abstruction;
+using E_Commerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Application.Services.Baskets
+{
+    public class BasketService : IBasketService
+    {
+        private readonly IApplicationDbContext _context;
+
+        public BasketService(IApplicationDbContext context)
+            => _context = context;
+
+        public async ValueTask<Basket> GetBasketAsync(int userTelegramId)
+        {
+            var basket = await _context.Baskets.FirstOrDefaultAsync(x => x.ClientTelegramId == userTelegramId);
+
+            if (basket == null)
+                throw new Exception("Basket not found");
+
+            return basket;
+        }
+
+        public async ValueTask<Basket> UpdateBasketProductsAsync(Product product, int count, int userTelegramId)
+        {
+            var basket = await _context.Baskets.FirstOrDefaultAsync(x => x.ClientTelegramId == userTelegramId);
+
+            if (basket == null)
+                throw new Exception("Basket not found");
+
+            var productWithCount = new Tuple<int, Product>(count,product);
+            basket.Products.Add(productWithCount);
+            var entry = _context.Baskets.Update(basket);
+            await _context.SaveChangesAsync();
+
+            return entry.Entity;
+        }
+
+        public async ValueTask<Basket> UpdateBasketStatusAsync(int userTelegramId)
+        {
+            var basket = await _context.Baskets.FirstOrDefaultAsync(x => x.ClientTelegramId == userTelegramId);
+
+            if (basket == null)
+                throw new Exception("Basket not found");
+
+            basket.IsActive = false;
+            var entry = _context.Baskets.Update(basket);
+            await _context.SaveChangesAsync();
+
+            return entry.Entity;
+        }
+    }
+}
---
+++ src/E-Commerce.Application/Services/Baskets/IBasketService.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Domain.Entities;
+
+namespace E_Commerce.Application.Services.Baskets
+{
+    public interface IBasketService
+    {
+        public ValueTask<Basket> GetBasketAsync(int userTelegramId);
+        public ValueTask<Basket> UpdateBasketProductsAsync(Product product,int count, int userTelegramId);
+        public ValueTask<Basket> UpdateBasketStatusAsync(int userTelegramId);
+    }
+}
