ceee689d07418e1880949c716b88a5e25b9d1173
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-06 02:58:36 +0500

    Categories Created

---
+++ src/E-Commerce.Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set;}
+    }
+}
--- src/E-Commerce.Domain/Entities/Product.cs
+++ src/E-Commerce.Domain/Entities/Product.cs
@@ -8,6 +8,8 @@ namespace E_Commerce.Domain.Entities
         public string Name { get; set; }
         public decimal Price { get; set; }
         public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
 
         public string? ImagePath { get; set; }
     }
