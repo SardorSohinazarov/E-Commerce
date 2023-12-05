eabcb91d5a067eb44bdaf0234d0b57f43c1a3cbd
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-05 21:17:28 +0500

    Logging Implemented

--- src/E-Commerce.Bot/Program.cs
+++ src/E-Commerce.Bot/Program.cs
@@ -2,8 +2,10 @@ using E_Commerce.Application;
 using E_Commerce.Bot.BackgroundServices;
 using E_Commerce.Bot.BotServices;
 using E_Commerce.Infrastructure;
+using Serilog;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
+using TelegramSink;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,15 @@ builder.Services.AddSingleton<IUpdateHandler, UpdateHandlerService>();
 
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.TeleSink("6668691456:AAFVKAI9X2J4FUMX37yTzA2DwmGbdQZvKD4", "5617428170")
+    .WriteTo.File("log.txt")
+    .CreateLogger();
+
+builder.Logging.AddSerilog();
 
 var app = builder.Build();
 
