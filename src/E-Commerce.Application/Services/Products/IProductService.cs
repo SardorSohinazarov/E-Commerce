756e0a8bca699edc677fcf7a73df9abf563883b1
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-05 22:03:49 +0500

    Product Service implemented

---
+++ src/E-Commerce.Application/Services/Products/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Application.Services.Products
+{
+    public interface IProductService
+    {
+        public ValueTask<Product> GetByNameAsync(string name);
+        public ValueTask<List<Product>> GetProductsAsync();
+    }
+}
---
+++ src/E-Commerce.Application/Services/Products/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using E_Commerce.Application.Abstruction;
+using E_Commerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Application.Services.Products
+{
+    public class ProductService:IProductService
+    {
+        private readonly IApplicationDbContext _context;
+
+        public ProductService(IApplicationDbContext context)
+            => _context = context;
+
+        public async ValueTask<Product> GetByNameAsync(string name)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Name == name);
+
+            if (product == null)
+                throw new Exception("Product Not Found");
+
+            return product;
+        }
+        
+        public async ValueTask<List<Product>> GetProductsAsync()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return products;
+        }
+    }
+}
