8ba7a12655e3b4db9e3676285aa2b90633a65b74
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-05 22:03:09 +0500

    Order Service implemented

---
+++ src/E-Commerce.Application/Services/Orders/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using E_Commerce.Domain.Entities;
+
+namespace E_Commerce.Application.Services.Orders
+{
+    public interface IOrderService
+    {
+        ValueTask<Order> AddOrderAsync(Order order);
+    }
+}
---
+++ src/E-Commerce.Application/Services/Orders/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce.Application.Abstruction;
+using E_Commerce.Domain.Entities;
+
+namespace E_Commerce.Application.Services.Orders
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IApplicationDbContext _context;
+
+        public OrderService(IApplicationDbContext context)
+            => _context = context;
+
+        public async ValueTask<Order> AddOrderAsync(Order order)
+        {
+            var entry = await _context.Orders.AddAsync(order);
+            _context.SaveChangesAsync();
+
+            return entry.Entity;
+        }
+    }
+}
