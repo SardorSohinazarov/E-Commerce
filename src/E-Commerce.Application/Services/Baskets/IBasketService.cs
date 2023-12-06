8610df95933cc1321f778602ce9ceee0e61593f9
SardorSohinazarov <sardorstudent0618gmail.com>
2023-12-07 00:00:54 +0500

    Remove and Adding Unique

--- src/E-Commerce.Application/Services/Baskets/IBasketService.cs
+++ src/E-Commerce.Application/Services/Baskets/IBasketService.cs
@@ -7,5 +7,6 @@ namespace E_Commerce.Application.Services.Baskets
         public ValueTask<Basket> GetBasketAsync(long userTelegramId);
         public ValueTask<Basket> UpdateBasketProductsAsync(Product product, int count, long userTelegramId);
         public ValueTask<Basket> UpdateBasketStatusAsync(int userTelegramId);
+        public ValueTask DeleteProductFromName(string name, long id);
     }
 }
